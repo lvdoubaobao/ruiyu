3f74cceff908ce3085e77fb97472863b07091039
root <root@ip-172-31-21-88.us-east-2.compute.internal>
2020-08-20 16:38:39 +0800

    14.3

--- app/code/Mazentop/AdvancedReview/Helper/Image.php
+++ app/code/Mazentop/AdvancedReview/Helper/Image.php
@@ -73,50 +73,54 @@ class Image extends AbstractHelper
      * @return string
      */
     public function resize($image, $width = null, $height = null)
-     {       
+    {
 
-        $image =str_replace('pub/media/review/img/','', $image);        
+        $image = str_replace('pub/media/review/img/', '', $image);
         $mediaFolder = self::DIRECTORY;
-        
         $absolutePath = $this->_mediaDirectory->getAbsolutePath($mediaFolder) . $image;
-        $imageinfo = getimagesize($absolutePath);
-        $width =  $imageinfo[0];
-        $height = $imageinfo[1];
-        //TODO resize width and height 1000        
-        if($width > 300){
-            $newWidth = 300;
-            $height = intval($newWidth * $height / $width);
-            $width = $newWidth;
-        }
-        $path = $mediaFolder . 'cache';
-        if ($width !== null) {
-            $path .= '/' . $width . 'x';
-            if ($height !== null) {
-                $path .= $height ;
+        //todo 这里判断图片是否存在
+        if ( file_exists($absolutePath)) {
+            $imageinfo = getimagesize($absolutePath);
+            $width = $imageinfo[0];
+            $height = $imageinfo[1];
+            //TODO resize width and height 1000
+            if ($width > 300) {
+                $newWidth = 300;
+                $height = intval($newWidth * $height / $width);
+                $width = $newWidth;
             }
-        }
-        $imageResized = $this->_mediaDirectory->getAbsolutePath($path) . $image;
-        try {
-            if (!$this->_fileExists($path . $image)) {
-                $imageFactory = $this->_imageFactory->create();
-                $imageFactory->open($absolutePath);
-                $imageFactory->constrainOnly(true);
-                $imageFactory->keepTransparency(true);
-                $imageFactory->keepFrame(true);
-                $imageFactory->keepAspectRatio(true);
-                $imageFactory->backgroundColor($this->_backgroundColor);
-                $imageFactory->quality($this->_quality);  
-                $imageFactory->resize($width, $height);
-                $imageFactory->save($imageResized);
+            $path = $mediaFolder . 'cache';
+            if ($width !== null) {
+                $path .= '/' . $width . 'x';
+                if ($height !== null) {
+                    $path .= $height;
+                }
             }
-            return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . $path . $image;
-        } catch (\Exception $e) {
-            $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
-            $logger = new \Zend\Log\Logger();
-            $logger->addWriter($writer);
-            $logger->info(var_export($e->getMessage(),true));
-        }       
-        return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'review/img/' . $image;
-        
+            $imageResized = $this->_mediaDirectory->getAbsolutePath($path) . $image;
+            try {
+                if (!$this->_fileExists($path . $image)) {
+                    $imageFactory = $this->_imageFactory->create();
+                    $imageFactory->open($absolutePath);
+                    $imageFactory->constrainOnly(true);
+                    $imageFactory->keepTransparency(true);
+                    $imageFactory->keepFrame(true);
+                    $imageFactory->keepAspectRatio(true);
+                    $imageFactory->backgroundColor($this->_backgroundColor);
+                    $imageFactory->quality($this->_quality);
+                    $imageFactory->resize($width, $height);
+                    $imageFactory->save($imageResized);
+                }
+                return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . $path . $image;
+            } catch (\Exception $e) {
+                $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
+                $logger = new \Zend\Log\Logger();
+                $logger->addWriter($writer);
+                $logger->info(var_export($e->getMessage(), true));
+            }
+            return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'review/img/' . $image;
+
+        }else{
+            return '';
+        }
     }
 }
